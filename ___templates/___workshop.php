bb77b25a096cbe90e7936920c091fb6fc0e4a6a1
Santhosh <santhosh22032004@gmail.com>
2023-01-25 18:00:01 +0000

    day1

---
+++ ___templates/___workshop.php
@@ -0,0 +1,22 @@
+<div class = "center">
+    <h2>DAY 1 : (10/02/2023)</h2>
+    <!-- <br> -->
+</div>
+<div  class="d-flex justify-content-center">
+    <div class="col-md-6">
+        <div class="card mb-3">
+            <img src="___sources/aiml.jpg" class="card-img-top" >
+            <div class="card-body">
+                <div class="center">
+                    <h4 class="card-title">WORKSHOP</h4>
+                    <h5>Artificial Intelligence & Machine Learning</h5>
+                    <div class="d-grid gap-2 d-md-block">
+                        <button class="btn btn-primary" onclick="window.location.href = '_ref.php';"type="button">Click here to know more</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
