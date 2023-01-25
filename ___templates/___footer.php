eb7023e0bc5fbe64f0964fd865c7409481f550e6
Santhosh <santhosh22032004@gmail.com>
2023-01-25 17:08:19 +0000

    reg

--- ___templates/___footer.php
+++ ___templates/___footer.php
@@ -1,15 +1,3 @@
-<!-- <main>
-<footer class="text-muted py-5">
-	<div class="container">
-		<p class="float-end mb-1">
-			<a href="#">Back to top</a>
-		</p>
-		<!-- <p class="mb-0">Website by Santhosh <a href="https://www.instagram.com/santhoshvairamuthu/">Follow me on
-				instagram</a></p> -->
-	</div>
-</footer>
-</main> -->
-
 <footer class="footer-distributed">
 
 			<div class="footer-left">
--- ___templates/___register.php
+++ ___templates/___register.php
@@ -1,7 +1,15 @@
-<div  class="align-middle">
+<div  class="d-flex justify-content-center"">
     <div class="col-md-6">
         <div class="h-100 p-5 bg-light border rounded-3">
-            <h2>Add borders</h2>
+            <div class="center">
+                <h2 >Register here !</h2><br>
+            </div>
+            <div style="clear: both">
+                <h4 style="display: inline-block" >Registration fee&nbsp;&nbsp;</h4>
+                <h3 style="display: inline-block" > ₹250/- </h3>
+                <h5 style="display: inline-block" >&nbsp;&nbsp;per head  </h5>
+            </div>
+            <br>
             <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
             <button class="btn btn-outline-secondary" type="button">Example button</button>
         </div>
