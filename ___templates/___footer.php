827244ca5ef48d355909b160b5feea06db853730
Santhosh <santhosh22032004@gmail.com>
2023-01-25 17:27:09 +0000

    register button added

--- ___templates/___footer.php
+++ ___templates/___footer.php
@@ -1,50 +1,34 @@
 <footer class="footer-distributed">
 
 			<div class="footer-left">
-
 				<h3>Company<span>logo</span></h3>
-
 				<p class="footer-links">
 					<a href="#" class="link-1">
 				</p>
-
 				<p class="footer-company-name">
 			</div>
-
 			<div class="footer-center">
-
 				<div>
 					<i class="fa fa-map-marker"></i>
 					<p><span>National Engineering College (Autonomous)<span>,
 K.R.Nagar, Kovilpatti – 628 503.</span> </p>
 				</div>
-
 				<div>
 				</div>
-
 				<div>
 					<i class="fa fa-envelope"></i>
 					<p><a href="mailto:reinox2022@gmail.com">reinox2022@gmail.com</a></p>
 				</div>
-
 			</div>
-
 			<div class="footer-right">
-
 				<p class="footer-company-about">
 					<span>About the company</span>
 					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
 				</p>
-
 				<div class="footer-icons">
-
 					<a href="#"><i class="fa fa-instagram"></i></a>
 					<a href="#"><i class="fa fa-twitter"></i></a>
 					<a href="#"><i class="fa fa-linkedin"></i></a>
-
-
 				</div>
-
 			</div>
-
 		</footer>
