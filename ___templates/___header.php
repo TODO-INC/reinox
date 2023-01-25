734d73862f638628586e9cf8db1beb7c4f84e209
Santhosh <santhosh22032004@gmail.com>
2023-01-25 16:42:59 +0000

    reg

--- ___templates/___header.php
+++ ___templates/___header.php
@@ -3,7 +3,7 @@
     <div class="container">
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
             <a class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                <img width="80" height="80" src="___sources/nec.png">
+                <img width="75" height="75" src="___sources/nec.png">
             </a>
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
