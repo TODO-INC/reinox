9e99cfcb5b377736e943c7e0a172a10fb3e0b7d6
yogaraj-stack <yogarajyogaraj178@gmail.com>
2023-01-26 00:43:57 +0530

    footer update and changes

--- ___templates/___footer.php
+++ ___templates/___footer.php
@@ -1,7 +1,8 @@
 <footer class="footer-distributed">
 
 			<div class="footer-left">
-				<h3>Company<span>logo</span></h3>
+				<!-- <h3>Company<span>logo</span></h3> -->
+				<img src="___sources/footer.png" alt="NEC IT">
 				<p class="footer-links">
 					<a href="#" class="link-1">
 				</p>
@@ -22,19 +23,26 @@ K.R.Nagar, Kovilpatti – 628 503.</span> </p>
 			</div>
 			<div class="footer-right">
 				<p class="footer-company-about">
-					<span>About the company</span>
-					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+					<span>IT ASSOCIATION</span>
+					<!-- <a href="  #">back to top</a> -->
+					
 				</p>
+				<a href="  #">back to top</a>
 				<div class="footer-icons">
 
 					<a href="https://instagram.com/necitsm?igshid=YmMyMTA2M2Y="target="blank"><i class="bi bi-instagram"></i></a>
 					<a href="https://twitter.com/NECKVP" target="blank"><i class="bi bi-twitter"></i></a>
 					<a href="https://www.linkedin.com/school/national-engineering-college/" target="blank"><i class="bi bi-linkedin"></i></a>
 					<a href="https://www.youtube.com/watch?v=5P7Iax-D4K4" target="blank"><i class="bi bi-youtube"></i></a>
+					
 
 
 				</div>
+
 			</div>
+			<!-- <div class="footer right">
+				<a href="#">Back to top</a>
+			</div> -->
 		</footer>
 
 		
