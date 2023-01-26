859d35a657b51ad625012c5d5e91036802eb5754
Santhosh <santhosh22032004@gmail.com>
2023-01-26 16:02:24 +0000

    solved

---
+++ ___templates/___footer.php
@@ -0,0 +1,47 @@
+<footer class="footer-distributed">
+
+			<div class="footer-left">
+				<!-- <h3>Company<span>logo</span></h3> -->
+				<!-- <img src="___sources/footer.png" alt="NEC IT">
+				<p class="footer-links">
+					<a href="#" class="link-1">
+				</p>
+				<p class="footer-company-name"> -->
+			</div>
+			<div class="footer-center">
+				<div>
+				<a href="https://goo.gl/maps/8giEuYmvymr8yP767" target="blank"><i class="bi bi-geo-alt" ></i></a>
+					<p><span>National Engineering College (Autonomous)<span>,K.R.Nagar, Kovilpatti – 628 503.</span> </p>
+				</div>
+				<div>
+				</div>
+				<div>
+					<i class="bi bi-envelope"></i>
+					<p><a href="mailto:reinox2022@gmail.com">reinox2022@gmail.com</a></p>
+				</div>
+			</div>
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>IT ASSOCIATION</span>
+					<!-- <a href="  #">back to top</a> -->
+					
+				</p>
+				<a href="  #">back to top</a>
+				<div class="footer-icons">
+
+					<a href="https://instagram.com/necitsm?igshid=YmMyMTA2M2Y="target="blank"><i class="bi bi-instagram"></i></a>
+					<a href="https://twitter.com/NECKVP" target="blank"><i class="bi bi-twitter"></i></a>
+					<a href="https://www.linkedin.com/school/national-engineering-college/" target="blank"><i class="bi bi-linkedin"></i></a>
+					<a href="https://www.youtube.com/@nationalengineeringcollege4252/featured" target="blank"><i class="bi bi-youtube"></i></a>
+					
+
+
+				</div>
+
+			</div>
+			<!-- <div class="footer right">
+				<a href="#">Back to top</a>
+			</div> -->
+		</footer>
+
+		
