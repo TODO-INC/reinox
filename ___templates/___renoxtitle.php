42c0a33ff4b9aade43fd249c5a3c0bc07665c58b
Santhosh <santhosh22032004@gmail.com>
2023-01-26 10:15:06 +0000

    junbotron

--- ___templates/___renoxtitle.php
+++ ___templates/___renoxtitle.php
@@ -1,9 +1,10 @@
-<div class="p-5 mb-4 bg-light rounded-3">
-    <div class="container-fluid py-5">
-        <div class="center">
-            <h1 class="display-5 fw-bold" style="font-family:baskervillie">IT ASSOCIATION</h1>
-            <h4  class="subtitle" style="font-family:cursive;">Proudly Presents</h4>
-            <img class="img2" width="200px" height="800px" src="___sources/reinox.png">
-        </div>
+<div class="container topt center"> 
+    <div class ="topt center">
+        <br>
+<div class="jumbotron myBackground center" style="background-color:transparent;">
+        <h1 class="whitney" style="text-align:center">IT ASSOCIATION</h1>
+        <h3 class="gotham" style="font-family:cursive; text-align:center">Proudly Presents</h3>
+        <img class="img2" width="200px" height="800px" src="___sources/reinox.png">
+    </div>
     </div>
 </div>
