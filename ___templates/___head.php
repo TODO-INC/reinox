9c65fe00fd7245d77fd35ae11dfcaa683268789b
Santhosh <santhosh22032004@gmail.com>
2023-01-26 08:40:14 +0000

    clear

--- ___templates/___head.php
+++ ___templates/___head.php
@@ -9,11 +9,7 @@
 	<!-- Core CSS -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 	<link rel="stylesheet" href="css/style.css">
-<<<<<<< HEAD
 	<link rel="stylesheet" href="css/button.css">
-=======
-
->>>>>>> 7055f7c70de8f7653732ff872aa2c81e38a97ee9
 	<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.1.0/css/hover-min.css">
 	<link href="css/<?=basename($_SERVER['PHP_SELF'], ".php")?>.css" rel="stylesheet">
@@ -21,16 +17,12 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-<<<<<<< HEAD
 	<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
 	<script src="___src/scripts.js"></script>
-=======
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">  
->>>>>>> 7055f7c70de8f7653732ff872aa2c81e38a97ee9
-  </head>
-	
 </head>
+	
