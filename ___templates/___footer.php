8d0ac54c04c6b2015f5b5a6a510b6161a4b3a03d
Santhosh <santhosh22032004@gmail.com>
2023-01-26 17:02:59 +0000

    try

--- ___templates/___footer.php
+++ ___templates/___footer.php
@@ -1,44 +1,24 @@
 <footer class="footer-distributed">
 
-			<div class="footer-left">
-			</div>
-			<div class="main-container justify-content-left ">
-			<div class="footer-center" >
-				<div>
-				<a href="https://goo.gl/maps/8giEuYmvymr8yP767" target="blank"><i class="bi bi-geo-alt" ></i></a>
-					<p><span>National Engineering College (Autonomous)<span>,K.R.Nagar, Kovilpatti – 628 503.</span> </p>
-				</div>
-				<div>
-				</div>
-				<div>
-					<i class="bi bi-envelope"></i>
-					<p><a href="mailto:reinox2022@gmail.com">reinox2022@gmail.com</a></p>
-				</div>
-			</div>
+<div class="col-md-4 d-flex align-items-center">
+	<a href="https://goo.gl/maps/8giEuYmvymr8yP767" target="blank"><i class="bi bi-geo-alt" ></i></a>
+	<p><span>National Engineering College (Autonomous)<span>,K.R.Nagar, Kovilpatti – 628 503.</span> </p>
+	<i class="bi bi-envelope"></i>
+	<p><a href="mailto:reinox2022@gmail.com">reinox2022@gmail.com</a></p>
 </div>
-<div class="main-container d-flex  justify-content-right ">
-			<div class="footer-right justify-content-right">
-				<p class="footer-company-about" >
-					<span>IT ASSOCIATION</span>
-					
-				</p>
-				<a href="  #">back to top</a>
-				<div class="footer-icons">
-
-					<a href="https://instagram.com/necitsm?igshid=YmMyMTA2M2Y="target="blank"><i class="bi bi-instagram"></i></a>
-					<a href="https://twitter.com/NECKVP" target="blank"><i class="bi bi-twitter"></i></a>
-					<a href="https://www.linkedin.com/school/national-engineering-college/" target="blank"><i class="bi bi-linkedin"></i></a>
-					<a href="https://www.youtube.com/@nationalengineeringcollege4252/featured" target="blank"><i class="bi bi-youtube"></i></a>
-					
+<div class="col-md-4 d-flex align-items-right">
+	<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+				<a href="#">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Back to top</a><br><br>
+	<div class="footer-icons">
+<br>
+<a href="https://instagram.com/necitsm?igshid=YmMyMTA2M2Y="target="blank"><i class="bi bi-instagram"></i></a>
+<a href="https://twitter.com/NECKVP" target="blank"><i class="bi bi-twitter"></i></a>
+<a href="https://www.linkedin.com/school/national-engineering-college/" target="blank"><i class="bi bi-linkedin"></i></a>
+<a href="https://www.youtube.com/@nationalengineeringcollege4252/featured" target="blank"><i class="bi bi-youtube"></i></a>
 
 
-				</div>
 
-			</div>
 </div>
-			<div class="footer right">
-				<a href="#">Back to top</a>
-			</div> 
-		</footer>
-
-		
+    </ul>
+</div>
+</footer>
