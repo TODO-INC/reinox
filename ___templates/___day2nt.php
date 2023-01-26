818801bea9d83cdf0c2e5e271f6b90b7fb543d53
Santhosh <santhosh22032004@gmail.com>
2023-01-26 13:27:13 +0000

    align

--- ___templates/___day2nt.php
+++ ___templates/___day2nt.php
@@ -2,7 +2,7 @@
     <h2>DAY 2 : (11/02/2023)</h2>
     <!-- <br> -->
     <h3>Non Technical Events</h3>
-</div>
+<!-- </div>
 <div  class="d-flex justify-content-center w-75 center">
   <div class="col-md-6">
     <div class="card mb-5">
@@ -40,14 +40,14 @@
             </div>
           </div>
         </div>
-        <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
-        </button> -->
+        </button>
         </div>
     <div class="card">
           <div class="card-body">
@@ -62,4 +62,51 @@
   </div>
 </div>
 <br>
+ -->
 
+
+ <div  class="d-flex justify-content-center">
+    <div class="col-md-6">
+        <div class="card mb-3">
+        <div id="carouselExampleAutoplaying" class="carousel slide " data-bs-ride="carousel" >
+        <div class="carousel-indicators h-25 ">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+          <br><br><br>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="___sources/picture1.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Dare to Prove</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="___sources/picture2.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Shout in Silence</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="___sources/picture3.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Screen Masters</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="___sources/picture4.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Shutter Up</h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-grid gap-2 d-md-block center">
+          <br><br>
+          <button class="btn btn-primary" onclick="window.location.href = 'd2t.php';"type="button" target="blank">Click here to know more</button>
+                    </div>
+      </div>
+        </div>
+    </div>
+</div>
--- ___templates/___day2t.php
+++ ___templates/___day2t.php
@@ -1,13 +1,14 @@
 <div class = "center">
     <h2>DAY 2 : (11/02/2023)</h2>
-    <!-- <br> -->
-    <h3>Technical Events</h3>
-</div>
-<div  class="d-flex justify-content-center w-75 center">
-  <div class="col-sm-6 ">
-    <div class="card mb-5">
-      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators h-25">
+    <h3>Technical Events</h3> 
+</div> 
+<!-- <div style="text-align:center" >
+<div  class="d-flex justify-content-center w-75 center" >
+  <div class="col-sm-6" >
+    <div class="center" >
+    <div class="card mb-5 " >
+      <div id="carouselExampleAutoplaying" class="carousel slide " data-bs-ride="carousel" >
+        <div class="carousel-indicators h-25 ">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
@@ -48,15 +49,16 @@
           </div>
         </div>
         
-        <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
-        </button> -->
-      
+        </button>
+      </div>
+    </div>
     </div>
     <div class="card">
           <div class="card-body">
@@ -72,4 +74,58 @@
   </div>
 </div>
 </div>
-<br>
+<br> -->
+
+
+<div  class="d-flex justify-content-center">
+    <div class="col-md-6">
+        <div class="card mb-3">
+        <div id="carouselExampleAutoplaying" class="carousel slide " data-bs-ride="carousel" >
+        <div class="carousel-indicators h-25 ">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+          <br><br><br>
+        </div>
+        <div class="carousel-inner ">
+          <div class="carousel-item active">
+          <img src="___sources/picture1.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Paper Persentation</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="___sources/picture2.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Coder Corner</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="___sources/picture3.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Tricked Bug</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="___sources/picture4.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Brain Teaser</h5>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="___sources/image.png" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Hozan ji</h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-grid gap-2 d-md-block center">
+          <br><br>
+          <button class="btn btn-primary" onclick="window.location.href = 'd2t.php';"type="button" target="blank">Click here to know more</button>
+                    </div>
+      </div>
+        </div>
+    </div>
+</div>
