a7825e9e12b185289762bbbb387485186b0411ea
Santhosh <santhosh22032004@gmail.com>
2023-01-27 02:33:12 +0000

    footer update

--- ___templates/___footer.php
+++ ___templates/___footer.php
@@ -1,4 +1,4 @@
-<footer class="footer-distributed">
+<!-- <footer class="footer-distributed">
 
 <div class="col-md-4 d-flex align-items-center">
 	<a href="https://goo.gl/maps/8giEuYmvymr8yP767" target="blank"><i class="bi bi-geo-alt" ></i></a>
@@ -18,4 +18,40 @@
 </div>
     </ul>
 </div>
-</footer>
+</footer> -->
+
+<footer class="footer-distributed">
+			<!-- <div class="main-container d-flex justify-content-left w-50 "> -->
+			<div class="footer-center justify-content-left">
+				<div>
+				<a href="https://goo.gl/maps/8giEuYmvymr8yP767" target="blank"><i class="bi bi-geo-alt"></i></a>
+					<p><span>National Engineering College (Autonomous)<span>,K.R.Nagar, Kovilpatti – 628 503.</span> </span></p>
+				</div>
+				<div>
+				</div>
+				<div>
+					<i class="bi bi-envelope"></i>
+					<p><a href="mailto:reinox2022@gmail.com">reinox2022@gmail.com</a></p>
+				</div>
+			</div>
+<!-- </div> -->
+<!-- <div class="main-container d-flex position-relative  justify-content-right right"> -->
+			<!-- <div class="footer-right justify-content-right"> -->
+			<div class="footer-right right">
+				<p class="footer-company-about">
+					<span>IT ASSOCIATION</span>
+					
+				</p>
+				<!-- <a href="  #">back to top</a> -->
+				<div class="footer-icons">
+					<a href="https://instagram.com/necitsm?igshid=YmMyMTA2M2Y=" target="blank"><i class="bi bi-instagram"></i></a>
+					<a href="https://twitter.com/NECKVP" target="blank"><i class="bi bi-twitter"></i></a>
+					<a href="https://www.linkedin.com/school/national-engineering-college/" target="blank"><i class="bi bi-linkedin"></i></a>
+					<a href="https://www.youtube.com/@nationalengineeringcollege4252/featured" target="blank"><i class="bi bi-youtube"></i></a>
+				</div>
+			<!-- </div> -->
+			<div class="footer-right w-50">
+				<a href="#">Back to top</a>
+			</div> 
+		</div>
+		</footer>
