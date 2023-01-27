5d9a4bb8d1893afdbf403321348cac6a845262a1
yogaraj-stack <yogarajyogaraj178@gmail.com>
2023-01-27 15:51:03 +0530

    yoga

--- test.php
+++ test.php
@@ -22,7 +22,7 @@ include "___libs/loads.php";
     </header>
 
     <main>
-    <?php load_temp("___register.php"); ?>
+    <?php load_temp("___workshop.php"); ?>
     </main>
 
     <?php load_temp("___footer.php"); ?>
