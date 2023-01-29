b76bfa9ce26c541b2d30e7a96f3b1122be33fede
Santhosh <santhosh22032004@gmail.com>
2023-01-29 13:21:15 +0000

    workshop

--- ___templates/___workshop.php
+++ ___templates/___workshop.php
@@ -6,14 +6,13 @@
 </div>
 <br>
 
-<div class="main-container col-md-12 d-flex justify-content-center ">
+<!-- <div class="main-container col-md-12 d-flex justify-content-center ">
 <div  class="d-flex justify-content-center">
     <div class="col-md-12 boxed1">
         <div class="card mb-3 ">
         <img src="___sources/AI.jpg" class="card-img-top" style="background:cover; width:90%;">
             <div class="card-body">
                 <div class="center">
-                    <!-- <h4 class="card-title">WORKSHOP</h4> -->
                     <h4 style="font-family:arvo;font-size: 4vh;color: rebeccapurple;">Technical Workshop on Artificial Intelligence &amp; Machine Learning</h4>
                     <div class="d-grid gap-2 d-md-block">
                         <button class="button2 b-green rot-135" onclick="window.location.href = 'd1ws.php';"type="button" target="blank"style ="font-family: 'Roboto', sans-serif;">Click here to know more</button>
@@ -25,26 +24,14 @@
 </div>
 </div>
 <br>
-<br>
-<!-- <button class="btn btn-primary" onclick="window.location.href = 'd1ws.php';"type="button">Click here to know more</button> -->
-                        <!-- <a href='https://docs.google.com/forms/d/e/1FAIpQLSc64S9mYQalnCJQrNWZSHw1oJjHautIrxIQC-QHnhtNtijlQA/viewform';" style ="font-family: 'Roboto', sans-serif;"class="button2 b-green rot-135">Click to register</a>
-                    </div> -->
-                        <!-- <button class="btn btn-primary" onclick="window.location.href = '_d1ws.php';"type="button">Click here to know more</button> -->
-<!-- <div class="center">
-    <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-            <a href="">
-                <div class="card-flyer">
-                    <div class="text-box">
-                        <div class="image-box">
-                            <img src="https://cdn.pixabay.com/photo/2018/03/30/15/11/deer-3275594_960_720.jpg" alt="" />
-                        </div>
-                        <div class="text-container">
-                            <h6>Title 01</h6>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </div>
-    </div>
-</div> -->
+<br> -->
+
+<div class="works">
+<article class="product-card">
+  <img class="product-card__image"src="___sources/AI.jpg" />
+  <div class="product-card__content">
+    <h1 class="product-card__title center"><br>WORKSHOP</h1>
+    <p class="product-card__text">Technical Workshop on Artificial Intelligence &amp; Machine Learning</p>
+      <button class="form-button" onclick="window.location.href = 'd1ws.php';"type="button" target="blank">Click here to know more</button>
+  </div>
+  </div>
