b8bffb27950f568b602d118130b2d926cd8d1415
Santhosh <santhosh22032004@gmail.com>
2023-01-31 19:26:12 +0000

    semi finals

---
+++ ___details/nt1.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html>
+	<head>
+  <link href='https://fonts.googleapis.com/css?family=Reem Kufi' rel='stylesheet'>
+  </head>
+	<body>
+	<div class="page">
+  <div class="timeline">
+    <div class="timeline__group">
+      <span class="timeline__year time" aria-hidden="true">Dare To Prove</span>
+      <div class="timeline__cards">
+        <div class="timeline__card card">
+          <header class="card__header">
+            
+          </header>
+          <div class="card__content">
+            <p>Everyone remembers the childhood memories when we played truth or dare with a group of friends. This time, you have a list of funny dares to make the game.Dare to prove is a fun filled event in which participants are asked to complete the chosen dare and there are many  funny dares that are guaranteed to make your jaw drop!.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="timeline__group">
+      <span class="timeline__year time" aria-hidden="true">RULES</span>
+      <div class="timeline__cards">
+        <div class="timeline__card card">
+          <header class="card__header">
+          </header>
+          <div class="card__content">
+            <p>All participants should perform individually.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="timeline__group">
+      <span class="timeline__year time" aria-hidden="true">Date</span>
+      <div class="timeline__cards">
+        <div class="timeline__card card">
+          <header class="card__header">          
+          </header>
+          <div class="card__content">
+            <p>11th FEB 2023</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+
+
+.r-title{
+  margin-top: var(--rTitleMarginTop, 0) !important;
+  margin-bottom: var(--rTitleMarginBottom, 0) !important;
+}
+
+
+p:not([class]){
+  line-height: var(--cssTypographyLineHeight, 1.78);
+  margin-top: var(--cssTypographyBasicMargin, 1em);
+  margin-bottom: 0;
+}
+
+p:not([class]):first-child{
+  margin-top: 0;
+}
+
+/*
+text component
+*/
+
+.text{
+  display: var(--textDisplay, inline-flex);
+  font-size: var(--textFontSize, 1rem);  
+}
+
+/*
+time component
+*/
+
+/*
+core styles
+*/
+
+.time{
+  display: var(--timeDisplay, inline-flex);
+}
+
+/*
+extensions
+*/
+
+.time__month{
+  margin-left: var(--timelineMounthMarginLeft, .25em);
+}
+
+/*
+skin
+*/
+
+.time{
+  padding: var(--timePadding, .25rem 1.25rem .25rem);
+  background-color: var(--timeBackgroundColor, #f0f0f0);
+
+  font-size: var(--timeFontSize, .75rem);
+  font-weight: var(--timeFontWeight, 700);
+  text-transform: var(--timeTextTransform, uppercase);
+  color: var(--timeColor, currentColor);
+}
+
+/*
+card component
+*/
+
+/*
+core styles
+*/
+
+.card{
+  padding: var(--timelineCardPadding, 1.5rem 1.5rem 1.25rem);
+}
+
+.card__content{
+  margin-top: var(--cardContentMarginTop, .5rem);
+  font-family: 'Reem Kufi';
+  
+}
+
+
+.card{
+  border-radius: var(--timelineCardBorderRadius, 2px);
+  border-left: var(--timelineCardBorderLeftWidth, 3px) solid var(--timelineCardBorderLeftColor, var(--uiTimelineMainColor));
+  box-shadow: var(--timelineCardBoxShadow, 0 1px 3px 0 rgba(0, 0, 0, .12), 0 1px 2px 0 rgba(0, 0, 0, .24));
+  background-color: var(--timelineCardBackgroundColor, #fff);
+}
+
+
+
+.card__title{
+  --rTitleMarginTop: var(--cardTitleMarginTop, 1rem);
+  font-size: var(--cardTitleFontSize, 1.25rem);
+}
+
+
+
+.timeline{
+  display: var(--timelineDisplay, grid);
+  grid-row-gap: var(--timelineGroupsGap, 2rem);
+}
+
+/*
+1. If timeline__year isn't displaed the gap between it and timeline__cards isn't displayed too
+*/
+
+.timeline__year{
+  margin-bottom: 1.25rem; /* 1 */
+  font-family: 'Reem Kufi';
+  
+}
+
+.timeline__cards{
+  display: var(--timeloneCardsDisplay, grid);
+  grid-row-gap: var(--timeloneCardsGap, 1.5rem);
+}
+
+
+/*
+=====
+SKIN
+=====
+*/
+
+.timeline{
+  --uiTimelineMainColor: var(--timelineMainColor, #222);
+  --uiTimelineSecondaryColor: var(--timelineSecondaryColor, #fff);
+
+  border-left: var(--timelineLineWidth, 3px) solid var(--timelineLineBackgroundColor, var(--uiTimelineMainColor));
+  padding-top: 1rem;
+  padding-bottom: 1.5rem;
+}
+
+.timeline__year{
+  --timePadding: var(--timelineYearPadding, .5rem 1.5rem);
+  --timeColor: var(--uiTimelineSecondaryColor);
+  --timeBackgroundColor: var(--uiTimelineMainColor);
+  --timeFontWeight: var(--timelineYearFontWeight, 400);
+}
+
+.timeline__card{
+  position: relative;
+  margin-left: var(--timelineCardLineGap, 1rem);
+}
+
+/*
+1. Stoping cut box shadow
+*/
+
+.timeline__cards{
+  overflow: hidden;
+  padding-top: .25rem; /* 1 */
+  padding-bottom: .25rem; /* 1 */
+}
+
+.timeline__card::before{
+  content: "";
+  width: 100%;
+  height: var(--timelineCardLineWidth, 2px);
+  background-color: var(--timelineCardLineBackgroundColor, var(--uiTimelineMainColor));
+
+  position: absolute;
+  top: var(--timelineCardLineTop, 1rem);
+  left: -50%;
+  z-index: -1;
+}
+
+/*
+=====
+SETTINGS
+=====
+*/
+/**/
+
+
+/*
+=====
+DEMO
+=====
+*/
+
+body{
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Open Sans, Ubuntu, Fira Sans, Helvetica Neue, sans-serif;
+  color: #222;
+
+  background-color:#00A4CCFF;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+}
+
+p{
+  margin-top: 0;
+  margin-bottom: 1rem;
+  line-height: 1.5;
+}
+
+p:last-child{
+  margin-bottom: 0;
+}
+
+.page{
+  max-width: 47rem;
+  padding: 5rem 2rem 3rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+
+
+</style>
+
+	</body>
+	
+</html>
--- ___templates/___events.php
+++ ___templates/___events.php
@@ -1,11 +1,11 @@
 <div class="fluid-container center d2t">
   <div >
-  <div class="evetite" style="color: #101820ff;text-shadow: 5px 5px 0px #f2aa4cff;">DAY 2 - 11th FEB 2k23</div>
-  <h2 class="headerx" style="color: #9c00ff;background-image: -webkit-linear-gradient(92deg, #13ffff, #ac00ff);">TECHNICAL EVENTS</h2><br>
+  <div class="evetite" style="color: #101820ff;text-shadow: 5px 5px 0px #f2aa4cff;">DAY 2 - 11th FEB 2k23</div><br><br>
+  <h2 class="headerx" style="color: #9c00ff;background-image: -webkit-linear-gradient(92deg, #13ffff, #ac00ff);">TECHNICAL EVENTS</h2><br><br>
   
     <div class="row eve card-grids center">
       <div class="col-md-2 col-sm-4 col-xs-6 width-99 center">
-        <a class="cardy" href="#">
+        <a class="cardy" href="___details/t5.php">
           <div class="card__background" style="background-image: url(___sources/t1.png)"></div>
           <div class="card__content">
             <p class="card__heading">Paper Presentation</p>
@@ -15,7 +15,7 @@
     
     
       <div class="col-md-2 col-sm-4  col-xs-6 width-99 center">
-        <a class="cardy" href="#">
+        <a class="cardy" href="___details/t1.php">
         <div class="card__background" style="background-image: url(___sources/t2.png)"></div>
         <div class="card__content">
           <p class="card__heading">Code Corner</p>
@@ -25,7 +25,7 @@
     
     
       <div class="col-md-2 col-sm-4 col-xs-6 width-99 center">
-      <a class="cardy" href="#">
+      <a class="cardy" href="___details/t2.php">
         <div class="card__background" style="background-image: url(___sources/t3.png)"></div>
         <div class="card__content">
           <p class="card__heading">Tricked Bug</p>
@@ -36,7 +36,7 @@
     
     
       <div class="col-md-2 col-sm-4 col-xs-6 width-99 center">
-      <a class="cardy" href="#">
+      <a class="cardy" href="___details/t4.php">
         <div class="card__background" style="background-image: url(___sources/t4.png)"></div>
         <div class="card__content">
           <p class="card__heading">Brain Teaser</p>
@@ -46,7 +46,7 @@
     
     
       <div class="col-md-2 col-sm-4 col-xs-6 width-99 center">
-      <a class="cardy" href="#">
+      <a class="cardy" href="___details/t3.php">
         <div class="card__background" style="background-image: url(___sources/t5.png)"></div>
         <div class="card__content">
           <p class="card__heading">Hozan Ji</p>
@@ -56,12 +56,12 @@
   
     </div>
 
-
+    <br><br><br>
     <div class="row eve card-grids center">
- <br> <h2 class="headerx " style="color: #2637ff;background-image: -webkit-linear-gradient(92deg, #00ff58, #2637ff);">NON TECHNICAL EVENTS</h2>      
+ <br> <h2 class="headerx " style="color: #2637ff;background-image: -webkit-linear-gradient(92deg, #00ff58, #2637ff);">NON TECHNICAL EVENTS</h2> <br><br>     
 
       <div class="col-md-2 col-sm-4 col-xs-6 center">
-        <a class="cardy" href="#">
+        <a class="cardy" href="___details/nt2.php">
           <div class="card__background" style="background-image: url(___sources/nt2.png)"></div>
           <div class="card__content">
             <p class="card__heading">Shout in Silence</p>
@@ -70,7 +70,7 @@
       </div>
         
       <div class="col-md-2 col-sm-4 col-xs-6 center">
-        <a class="cardy" href="#">
+        <a class="cardy" href="___details/nt1.php">
           <div class="card__background" style="background-image: url(___sources/nt1.png)"></div>
           <div class="card__content">
             <p class="card__heading">Dare to Prove</p>
@@ -78,7 +78,7 @@
         </a>
       </div>
       <div class="col-md-2 col-sm-4 col-xs-6 center">
-        <a class="cardy" href="#">
+        <a class="cardy" href="___details/nt3.php">
           <div class="card__background" style="background-image: url(___sources/nt4.png)"></div>
           <div class="card__content">
             <p class="card__heading">Shutter Up</p>
@@ -86,7 +86,7 @@
         </a>
       </div>
       <div class="col-md-2 col-sm-4 col-xs-6 center">
-        <a class="cardy" href="#">
+        <a class="cardy" href="___details/nt4.php">
           <div class="card__background" style="background-image: url(___sources/nt3.png)"></div>
           <div class="card__content">
             <p class="card__heading">Screen Master</p>
