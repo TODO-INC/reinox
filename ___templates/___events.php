9dc9f7755743165da59404dc01ccd05ef5209e32
Santhosh <santhosh22032004@gmail.com>
2023-01-31 11:00:26 +0000

    some change

--- ___templates/___events.php
+++ ___templates/___events.php
@@ -1,9 +1,9 @@
 <div class="fluid-container center d2t">
   <div >
   <div class="evetite" style="color: #101820ff;text-shadow: 5px 5px 0px #f2aa4cff;">DAY 2 - 11th FEB 2k23</div>
-  <h1 class="headerx" style="color: #9c00ff;background-image: -webkit-linear-gradient(92deg, #13ffff, #ac00ff);">Technical events</h1><br>
+  <h2 class="headerx" style="color: #9c00ff;background-image: -webkit-linear-gradient(92deg, #13ffff, #ac00ff);">TECHNICAL EVENTS</h2><br>
   
-    <div class="row eve card-grids hero-sectionx center">
+    <div class="row eve card-grids center">
       <div class="col-md-2 col-sm-4 col-xs-6 width-99 center">
         <a class="cardy" href="#">
           <div class="card__background" style="background-image: url(___sources/t1.png)"></div>
@@ -14,7 +14,7 @@
       </div>
     
     
-      <div class="col-md-2 col-sm-4  width-99 center">
+      <div class="col-md-2 col-sm-4  col-xs-6 width-99 center">
         <a class="cardy" href="#">
         <div class="card__background" style="background-image: url(___sources/t2.png)"></div>
         <div class="card__content">
@@ -57,8 +57,8 @@
     </div>
 
 
-    <div class="row eve hero-sectionx card-grids center">
- <br> <h1 class="headerx " style="color: #2637ff;background-image: -webkit-linear-gradient(92deg, #00ff58, #2637ff);">Non Technical events</h1>      
+    <div class="row eve card-grids center">
+ <br> <h2 class="headerx " style="color: #2637ff;background-image: -webkit-linear-gradient(92deg, #00ff58, #2637ff);">NON TECHNICAL EVENTS</h2>      
 
       <div class="col-md-2 col-sm-4 col-xs-6 center">
         <a class="cardy" href="#">
